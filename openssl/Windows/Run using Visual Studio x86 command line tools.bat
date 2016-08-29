5ad26b01ba3956ba8a4b18fddf8b01848c45f055
Michael EL BAKI <michael.elbaki@gmail.com>
2016-08-29 19:24:24 +0200

    Modified scripts to compile openSSL on Windows, using 1.0.2h version.

--- openssl/Windows/Run using Visual Studio x86 command line tools.bat
+++ openssl/Windows/Run using Visual Studio x86 command line tools.bat
@@ -2,18 +2,18 @@
 set here=%~dp0
 cd %here%\..
 @echo Should anything go wrong, check that you have ActivePerl on your system (instead of mingw/cygwin perl).
-@if exist openssl-1.0.1i (
+@if exist openssl-1.0.2h (
 	@echo Source already extracted, will simply rebuild
-	cd openssl-1.0.1i
+	cd openssl-1.0.2h
 	goto skipConfigure
 )
-@if not exist openssl-1.0.1i.tar.gz (
-	@echo You need to download openssl-1.0.1i.tar.gz from the official source and place it under the openssl directory.
+@if not exist openssl-1.0.2h.tar.gz (
+	@echo You need to download openssl-1.0.2h.tar.gz from the official source and place it under the openssl directory.
 	goto :eof
 )
 
-tar xzfv openssl-1.0.1i.tar.gz
-cd openssl-1.0.1i
+tar xzfv openssl-1.0.2h.tar.gz
+cd openssl-1.0.2h
 patch -p0 < ../Windows/tools.patch
 :: Might fail since patch is non standard on Windows
 @if %errorlevel%==9009 (
@@ -22,7 +22,8 @@ patch -p0 < ../Windows/tools.patch
 )
 
 perl Configure VC-WIN32 --prefix=Build-Win32
-call ms\do_ms
+::call ms\do_ms
+call ms\do_nasm.bat
 
 :skipConfigure
 nmake -f ms\nt.mak tmp32 out32 inc32\openssl headers lib
